d6971269e2e289d8733bb89e022f5998d80467e8
Webees Tech <webeestechfr@gmail.com>
2023-07-17 07:03:52 -0700

    last commit

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Abril_Fatface, Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const abril = Abril_Fatface({ 
+  weight: '400',
+  subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={abril.className}>{children}</body>
     </html>
   )
 }
